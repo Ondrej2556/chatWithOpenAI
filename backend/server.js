70421b2a6c53070f12f95b2c330351a8fc91181d
Ondřej Zahradník <ondrej@Ondrej-MacBook-Air.local>
2023-01-18 10:18:37 +0100

    update mac

--- backend/server.js
+++ backend/server.js
@@ -16,12 +16,13 @@ const db = {
 
 API_KEY = process.env.OPENAI_API
 app.post('/chat', function (req, res) {
+    console.log(API_KEY)
     const options = {
         method: 'POST',
         url: 'https://api.openai.com/v1/completions',
         headers: {
             'Content-Type': 'application/json',
-            'Authorization': `Bearer ${API_KEY}`
+            'Authorization': `${API_KEY}`
             //GET YOUR API KEY ON OFFICIAL OPENAI WEB
         },
         data: {
